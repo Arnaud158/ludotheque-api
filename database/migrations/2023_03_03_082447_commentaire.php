23370fd52489b10e2da60d6ce04308d4196698ef
Jules B <bobeuf.jules@gmail.com>
2023-03-03 09:30:47 +0100

    fin migration commentaire

---
+++ database/migrations/2023_03_03_082447_commentaire.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('Commentaire', function (Blueprint $table) {
+            $table->id();
+            $table->string('commentaire', 200);
+            $table->dateTime('date_com')->default(now());
+            $table->integer('note');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('Commentaire');
+    }
+};
